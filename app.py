f7d014abfbf0a818c8a93d61e0ef158df36a932c
Ray Beecham <raybeecham2009@gmail.com>
2026-04-27 02:19:43 -0500

    Initial Flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+# Flask app stub 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, DevSecOps!"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
